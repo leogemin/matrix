a2906a5fe29015a42d2e01f60bf6101b42346eca
leogemin <leonardo.gemin.pereira@gmail.com>
2025-03-18 10:48:08 -0300

    fix: suggestions and responsiveness

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -14,9 +14,10 @@ const Header = () => {
             <li className="header__top__menu__option">
                 <span>Help</span>
             </li>
-            <li className="header__top__menu__option">
-                <Button type="button" color="white" handleClick={() => {}} content="Sign in / Join" />
+            <li className="header__top__menu__drop">
+                <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
             </li>
+            <Button type="button" color="white" handleClick={() => {}} content="Sign in / Join" />
            </ul>
         </div>
         <nav className="header__nav">
